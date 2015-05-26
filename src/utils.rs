8773f970aefe15d9df6a17c7c69c6adf0fd5f2a6
Vladislav Supalov <vladislav@supalov.com>
2015-05-26 19:02:59 +0200

    Update of dependencies. Slight improvements. Less useless stuff.

--- src/utils.rs
+++ src/utils.rs
@@ -13,29 +13,27 @@ use cards::card::{Card, Value, Suit};
 ///  cdhs = suit of card
 ///  b = bit turned on depending on value of card
 pub fn card_to_deck_number(card: &Card) -> CactusKevCard {
-    let &Card(ref card_value, ref card_suit) = card;
-
-    let value : u32  = match card_value {
-        &Value::Two => 0,
-        &Value::Three => 1,
-        &Value::Four => 2,
-        &Value::Five => 3,
-        &Value::Six => 4,
-        &Value::Seven => 5,
-        &Value::Eight => 6,
-        &Value::Nine => 7,
-        &Value::Ten => 8,
-        &Value::Jack => 9,
-        &Value::Queen => 10,
-        &Value::King => 11,
-        &Value::Ace => 12,
+    let value : u32  = match card.value {
+        Value::Two => 0,
+        Value::Three => 1,
+        Value::Four => 2,
+        Value::Five => 3,
+        Value::Six => 4,
+        Value::Seven => 5,
+        Value::Eight => 6,
+        Value::Nine => 7,
+        Value::Ten => 8,
+        Value::Jack => 9,
+        Value::Queen => 10,
+        Value::King => 11,
+        Value::Ace => 12,
     };
     let prime : u32 = lookups::PRIMES[value as usize] as u32;
-    let suit : u32  = match card_suit {
-        &Suit::Hearts => 0x1000,
-        &Suit::Spades => 0x2000,
-        &Suit::Diamonds => 0x4000,
-        &Suit::Clubs => 0x8000,
+    let suit : u32  = match card.suit {
+        Suit::Hearts => 0x1000,
+        Suit::Spades => 0x2000,
+        Suit::Diamonds => 0x4000,
+        Suit::Clubs => 0x8000,
     };
     let bits : u32 = 1 << (16+value);
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -7,15 +7,13 @@ use cards::card::{Card, Value, Suit};
 use holdem::{HandRankClass};
 use holdem::{hand_rank_to_class};
 
-use holdem::{HandCards, CommunityCards, CardSlot};
-
 #[test]
 fn get_rank_of_5() {
-    let c1 = Card(Value::Two, Suit::Spades);
-    let c2 = Card(Value::Two, Suit::Hearts);
-    let c3 = Card(Value::Two, Suit::Diamonds);
-    let c4 = Card(Value::Two, Suit::Clubs);
-    let c5 = Card(Value::Three, Suit::Hearts);
+    let c1 = Card::new(Value::Two, Suit::Spades);
+    let c2 = Card::new(Value::Two, Suit::Hearts);
+    let c3 = Card::new(Value::Two, Suit::Diamonds);
+    let c4 = Card::new(Value::Two, Suit::Clubs);
+    let c5 = Card::new(Value::Three, Suit::Hearts);
 
     let cards = [&c1, &c2, &c3, &c4, &c5];
     let rank = pokereval::eval_5cards(&cards);
@@ -25,12 +23,12 @@ fn get_rank_of_5() {
 
 #[test]
 fn get_rank_of_6() {
-    let c1 = Card(Value::Two, Suit::Spades);
-    let c2 = Card(Value::Two, Suit::Hearts);
-    let c3 = Card(Value::Two, Suit::Diamonds);
-    let c4 = Card(Value::Two, Suit::Clubs);
-    let c5 = Card(Value::Three, Suit::Hearts);
-    let c6 = Card(Value::Three, Suit::Diamonds);
+    let c1 = Card::new(Value::Two, Suit::Spades);
+    let c2 = Card::new(Value::Two, Suit::Hearts);
+    let c3 = Card::new(Value::Two, Suit::Diamonds);
+    let c4 = Card::new(Value::Two, Suit::Clubs);
+    let c5 = Card::new(Value::Three, Suit::Hearts);
+    let c6 = Card::new(Value::Three, Suit::Diamonds);
 
     let cards = [&c1, &c2, &c3, &c4, &c5, &c6];
     let rank = pokereval::eval_6cards(&cards);
@@ -40,56 +38,16 @@ fn get_rank_of_6() {
 
 #[test]
 fn get_rank_of_7() {
-    let c1 = Card(Value::Two, Suit::Spades);
-    let c2 = Card(Value::Two, Suit::Hearts);
-    let c3 = Card(Value::Two, Suit::Diamonds);
-    let c4 = Card(Value::Two, Suit::Clubs);
-    let c5 = Card(Value::Three, Suit::Hearts);
-    let c6 = Card(Value::Three, Suit::Diamonds);
-    let c7 = Card(Value::Three, Suit::Clubs);
+    let c1 = Card::new(Value::Two, Suit::Spades);
+    let c2 = Card::new(Value::Two, Suit::Hearts);
+    let c3 = Card::new(Value::Two, Suit::Diamonds);
+    let c4 = Card::new(Value::Two, Suit::Clubs);
+    let c5 = Card::new(Value::Three, Suit::Hearts);
+    let c6 = Card::new(Value::Three, Suit::Diamonds);
+    let c7 = Card::new(Value::Three, Suit::Clubs);
 
     let cards = [&c1, &c2, &c3, &c4, &c5, &c6, &c7];
     let rank = pokereval::eval_7cards(&cards);
 
-    assert_eq!(hand_rank_to_class(&rank), HandRankClass::FourOfAKind);
-}
-
-
-#[test]
-fn get_rank_of_hand_and_community_cards() {
-    let hand = HandCards(
-        CardSlot::Dealt(Card(Value::Two, Suit::Spades)),
-        CardSlot::Dealt(Card(Value::Two, Suit::Hearts)),
-    );
-
-    let community = CommunityCards(
-        CardSlot::Dealt(Card(Value::Two, Suit::Clubs)),
-        CardSlot::Dealt(Card(Value::Two, Suit::Diamonds)),
-        CardSlot::Dealt(Card(Value::Three, Suit::Spades)),
-        CardSlot::Dealt(Card(Value::Three, Suit::Clubs)),
-        CardSlot::Dealt(Card(Value::Three, Suit::Hearts)),
-    );
-
-    let rank = pokereval::eval_for_player(&hand, &community);
-    assert_eq!(hand_rank_to_class(&rank), HandRankClass::FourOfAKind);
-}
-
-#[test]
-#[should_panic]
-fn get_rank_of_hand_and_community_cards_panic() {
-    let hand = HandCards(
-        CardSlot::Dealt(Card(Value::Two, Suit::Spades)),
-        CardSlot::Dealt(Card(Value::Two, Suit::Hearts)),
-    );
-
-    let community = CommunityCards(
-        CardSlot::Dealt(Card(Value::Two, Suit::Clubs)),
-        CardSlot::Dealt(Card(Value::Two, Suit::Diamonds)),
-        CardSlot::Dealt(Card(Value::Three, Suit::Spades)),
-        CardSlot::Dealt(Card(Value::Three, Suit::Clubs)),
-        CardSlot::Empty,
-    );
-
-    let rank = pokereval::eval_for_player(&hand, &community);
     assert_eq!(hand_rank_to_class(&rank), HandRankClass::FourOfAKind);
 }
